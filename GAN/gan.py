e2c1aaaaec3317abb029a9843c9991e3129b47f9
potyama <kamikaze117117@gmail.com>
2022-09-13 12:32:57 +0900

    add *.py

---
+++ GAN/gan.py
@@ -0,0 +1,182 @@
+### -*-coding:utf-8-*-
+from __future__ import print_function, division
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+
+import matplotlib.pyplot as plt
+
+import sys
+
+import numpy as np
+
+class GAN():
+    def __init__(self):
+        # datasize for mnist image set
+        self.img_rows = 28
+        self.img_cols = 28
+        self.channels = 1
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+
+        self.z_dim = 100
+
+        optimizer = Adam(0.0002, 0.5)
+
+        # discriminator model
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy',
+                                   optimizer=optimizer,
+                                   metrics=['accuracy'])
+
+        # Generator model
+        self.generator = self.build_generator()
+        # we don't need to compile generator because generator learns with descriminatorgenerator as 'combined'
+        #self.generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+        # we can define combined model by two ways (results are same)
+        # please see https://qiita.com/triwave33/items/1890ccc71fab6cbca87e
+        # sorry in Japanese
+        self.combined = self.build_combined1()
+        #self.combined = self.build_combined2()
+        self.combined.compile(loss='binary_crossentropy',
+                              optimizer=optimizer)
+
+    def build_generator(self):
+
+        noise_shape = (self.z_dim,)
+        model = Sequential()
+
+        model.add(Dense(256, input_shape=noise_shape))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(1024))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
+        model.add(Reshape(self.img_shape))
+
+        model.summary()
+
+        return model
+
+    def build_discriminator(self):
+
+        img_shape = (self.img_rows, self.img_cols, self.channels)
+
+        model = Sequential()
+
+        model.add(Flatten(input_shape=img_shape))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(256))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(1, activation='sigmoid'))
+        model.summary()
+
+        return model
+
+    def build_combined1(self):
+        self.discriminator.trainable = False
+        model = Sequential([self.generator, self.discriminator])
+        return model
+
+    def build_combined2(self):
+        z = Input(shape=(self.z_dim,))
+        img = self.generator(z)
+        self.discriminator.trainable = False
+        valid = self.discriminator(img)
+        model = Model(z, valid)
+        model.summary()
+        return model
+
+
+
+    def train(self, epochs, batch_size=128, save_interval=50):
+
+        (X_train, _), (_, _) = mnist.load_data()
+
+        # normalize
+        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+        X_train = np.expand_dims(X_train, axis=3)
+
+        half_batch = int(batch_size / 2)
+
+        num_batches = int(X_train.shape[0] / half_batch)
+        print('Number of batches:', num_batches)
+
+        for epoch in range(epochs):
+
+            for iteration in range(num_batches):
+
+                # ---------------------
+                #  Discriminator learning
+                # ---------------------
+
+                # pickup images (half-batch size) from generator 
+                noise = np.random.normal(0, 1, (half_batch, self.z_dim))
+                gen_imgs = self.generator.predict(noise)
+
+
+                # pickup images (half-batch size) from dataset
+                idx = np.random.randint(0, X_train.shape[0], half_batch)
+                imgs = X_train[idx]
+
+                # learn discriminator
+                # learning discriminator with real-data and fake-data seperately
+                d_loss_real = self.discriminator.train_on_batch(imgs, np.ones((half_batch, 1)))
+                d_loss_fake = self.discriminator.train_on_batch(gen_imgs, np.zeros((half_batch, 1)))
+                # average each loss
+                d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+
+                # ---------------------
+                #  Generator learning
+                # ---------------------
+
+                noise = np.random.normal(0, 1, (batch_size, self.z_dim))
+
+                # make label (gen data: 1) 
+                valid_y = np.array([1] * batch_size)
+
+                # Train the generator
+                g_loss = self.combined.train_on_batch(noise, valid_y)
+
+                # progress
+                print ("epoch:%d, iter:%d,  [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, iteration, d_loss[0], 100*d_loss[1], g_loss))
+
+                # save images 
+            if epoch % save_interval == 0:
+                self.save_imgs(epoch)
+
+    def save_imgs(self, epoch):
+        # row,col
+        r, c = 5, 5
+
+        noise = np.random.normal(0, 1, (r * c, self.z_dim))
+        gen_imgs = self.generator.predict(noise)
+
+        # rescale [-1, 1] to [0, 1]
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("images/mnist_%d.png" % epoch)
+        plt.close()
+
+
+if __name__ == '__main__':
+    gan = GAN()
+    gan.train(epochs=30000, batch_size=32, save_interval=1)
